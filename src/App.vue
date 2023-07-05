951a9e10f2d98f53f4e14b1babdf041ad99b0574
hasmikmejlumyan <hasik.mejlumyan@esterox.am>
2023-07-05 23:46:34 +0400

    [Lesson-9] added new components and created folders

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,37 @@
 <template>
   <div class="app">
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    >
-    </user-info>
-    <course-goals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-      </template>
-    </course-goals>
+
+    <button @click="setSelectedComponents('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponents('manage-goals')">Manage Goals</button>
+
+    <!-- <active-goals v-if="selectedComponents === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponents === 'manage-goals'"></manage-goals> -->
+
+    <keep-alive>
+      <component :is="selectedComponents"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
-
+import TheHeader from './components/layout/TheHeader.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
 
   data() {
     return {
+      selectedComponents: 'active-goals',
       activeUser: {
         name: 'Hasmik Mejlumyan',
         description: 'Site owner and admin',
@@ -40,6 +39,12 @@ export default {
       },
     };
   },
+
+  methods: {
+    setSelectedComponents(cmp) {
+      this.selectedComponents = cmp;
+    }
+  }
 };
 </script>
 
